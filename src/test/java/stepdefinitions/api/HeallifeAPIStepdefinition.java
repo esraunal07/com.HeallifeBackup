9a263037e357713c6e641f26d36499dfa9db979f
BerivanGN <berivangnc@gmail.com>
2023-05-23 15:47:03 +0300

    V3

--- src/test/java/stepdefinitions/api/HeallifeAPIStepdefinition.java
+++ src/test/java/stepdefinitions/api/HeallifeAPIStepdefinition.java
@@ -1,4 +1,37 @@
 package stepdefinitions.api;
 
+import hooks.api.HooksAPI;
+import io.cucumber.java.en.Given;
+import org.json.JSONObject;
+
 public class HeallifeAPIStepdefinition {
+
+    public static String fullPath;
+
+    JSONObject reqBodyJson;
+
+    @Given("Api kullanicisi {string} path parametreleri set eder")
+    public void api_kullanicisi_path_parametreleri_set_eder(String rawPath) {
+        String[] paths = rawPath.split("/");
+
+        StringBuilder tempPath = new StringBuilder("/{");
+
+        for (int i = 0; i < paths.length; i++) {
+
+            String key = "pp" + i; //pp0 pp1 pp2 ...
+            String value = paths[i].trim();
+
+            HooksAPI.spec.pathParam(key,value);
+
+            tempPath.append(key + "}/{");
+        }
+
+        tempPath.deleteCharAt(tempPath.lastIndexOf("{"));
+        tempPath.deleteCharAt(tempPath.lastIndexOf("/"));
+
+        // System.out.println("tempPath : " + tempPath);
+
+        fullPath = tempPath.toString();
+    }
+
 }
---
+++ src/test/resources/features/APIFeature/deneme.feature
@@ -0,0 +1,5 @@
+Feature: US_02 Bir yönetici olarak API baglantisi üzerinden Staff List'e erisebilmeliyim
+
+  Scenario: Bir yönetici olarak API baglantisi üzerinden Staff List'e erisebilmeliyim
+
+    Given Api kullanicisi "api/register" path parametreleri set eder
