2bfe2fe08660d03b8c2334c7d7f8e29b0ad4034b
BerivanGN <berivangnc@gmail.com>
2023-05-24 11:06:14 +0300

    BerivanUS2.24.05

--- src/test/java/stepdefinitions/api/HeallifeAPIStepdefinition.java
+++ src/test/java/stepdefinitions/api/HeallifeAPIStepdefinition.java
@@ -2,13 +2,23 @@ package stepdefinitions.api;
 
 import hooks.api.HooksAPI;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.json.JSONObject;
+import org.junit.Assert;
+
+import static io.restassured.RestAssured.given;
 
 public class HeallifeAPIStepdefinition {
 
     public static String fullPath;
 
     JSONObject reqBodyJson;
+    int basariliStatusKod= 200;
+    int basarisizStatusKod = 403;
+    Response response ;
+
 
     @Given("Api kullanicisi {string} path parametreleri set eder")
     public void api_kullanicisi_path_parametreleri_set_eder(String rawPath) {
@@ -34,4 +44,29 @@ public class HeallifeAPIStepdefinition {
         fullPath = tempPath.toString();
     }
 
+    @Then("Api kullanicisi staffList icin gonderdigi dogru get Request sonucunda donen status kodunun ikiyuz oldugunu dogrular")
+    public void apiKullanicisiStaffListIcinGonderdigiDogruGetRequestSonucundaDonenStatusKodununIkiyuzOldugunuDogrular() {
+    response = given()
+                .spec(HooksAPI.spec)
+                .headers("Authorization","Bearer " + HooksAPI.token)
+                .contentType(ContentType.JSON)
+                .header("Accept","application/json")
+                .when().get(fullPath);
+        response.prettyPrint();
+        //HeallifeMethods.getResponse(fullPath);
+        Assert.assertEquals(basariliStatusKod,response.getStatusCode());
+    }
+
+    @Then("Api kullanicisi staffList icin gonderdigi yanlis get Request sonucunda donen status kodunun dortyuzuc oldugunu dogrular")
+    public void apiKullanicisiStaffListIcinGonderdigiYanlisGetRequestSonucundaDonenStatusKodununDortyuzucOldugunuDogrular() {
+        //HeallifeMethods.getResponse(fullPath);
+        response = given()
+                .spec(HooksAPI.spec)
+                .headers("Authorization","Bearer " + HooksAPI.token)
+                .contentType(ContentType.JSON)
+                .header("Accept","application/json")
+                .when().get(fullPath);
+        response.prettyPrint();
+        Assert.assertEquals(basarisizStatusKod,response.getStatusCode());
+    }
 }
---
+++ src/test/resources/features/api/deneme.feature
@@ -0,0 +1,11 @@
+Feature: US_02 Bir yönetici olarak API baglantisi üzerinden Staff List'e erisebilmeliyim
+@wip
+  Scenario: Bir yönetici olarak API baglantisi üzerinden Staff List'e erisebilmeliyim
+
+    Given Api kullanicisi "api/staffList" path parametreleri set eder
+    Then Api kullanicisi staffList icin gonderdigi dogru get Request sonucunda donen status kodunun ikiyuz oldugunu dogrular
+
+  Scenario: /api/staffList endpoint'ine gecersiz authorization bilgileri ile bir GET Request gönderildiginde dönen status code'un 403 oldugu ve response message bilgisinin "failed" oldugu dogrulanmali
+
+    Given Api kullanicisi "api/staffList" path parametreleri set eder
+    Then Api kullanicisi staffList icin gonderdigi yanlis get Request sonucunda donen status kodunun dortyuzuc oldugunu dogrular
